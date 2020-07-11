6692c1f82a461e6b0c91e2073c098ba3108fb2e5
Raine Revere <raine@cybersemics.org>
2020-07-11 11:52:45 -0600

    Red pulse

--- app.js
+++ app.js
@@ -1,18 +1,11 @@
 var express = require('express')
 var bodyParser = require('body-parser')
-var favicon = require('serve-favicon')
 
 var app = express()
 app.set('view engine', 'jade')
 app.set('views', __dirname + '/views')
 app.use(express.static(__dirname + '/public'))
-app.use(favicon(__dirname + '/public/favicon.ico'))
 app.use(bodyParser())
-
-// accept all paths for client-side routing
-app.get('/calendar', function(req, res) {
-  res.render('calendar')
-})
 
 // accept all paths for client-side routing
 app.get('*', function(req, res) {
